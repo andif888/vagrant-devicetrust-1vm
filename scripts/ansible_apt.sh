3dbe60200559243353692415aabdde05c99f6a9d
andif888 <andif@snowbay.de>
2020-05-08 22:23:15 +0200

    pip3

--- scripts/ansible_apt.sh
+++ scripts/ansible_apt.sh
@@ -5,10 +5,7 @@ export LC_ALL=C
 
 sudo apt-get -y update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
-software-properties-common unzip git python3-dev python-pip python3-pip python-wheel \
+software-properties-common unzip git python3-dev python3-pip python3-wheel \
 openssh-client libssl-dev libkrb5-dev krb5-user
 
-# sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq --no-install-recommends krb5-user
-
-sudo pip install --upgrade pip
-sudo pip install ansible cryptography pywinrm kerberos requests_kerberos
+sudo pip3 install ansible cryptography pywinrm kerberos requests_kerberos
