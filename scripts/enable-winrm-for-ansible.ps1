28dbbaca473a435c2b1df40c64af84e87a5893ed
andif888 <andif@snowbay.de>
2020-05-08 22:24:55 +0200

    optimize powershell using native images

--- scripts/enable-winrm-for-ansible.ps1
+++ scripts/enable-winrm-for-ansible.ps1
@@ -1,2 +1,22 @@
 [Net.ServicePointManager]::SecurityProtocol = 'Tls12'
 iex ((New-Object System.Net.Webclient).DownloadString('https://github.com/ansible/ansible/raw/devel/examples/scripts/ConfigureRemotingForAnsible.ps1'))
+
+function Optimize-PowershellAssemblies {
+    # NGEN powershell assembly, improves startup time of powershell by 10x
+    $old_path = $env:path
+    try {
+      $env:path = [Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
+      [AppDomain]::CurrentDomain.GetAssemblies() | % {
+        if (! $_.location) {continue}
+        $Name = Split-Path $_.location -leaf
+        if ($Name.startswith("Microsoft.PowerShell.")) {
+          Write-Progress -Activity "Native Image Installation" -Status "$name"
+          ngen install $_.location | % {"`t$_"}
+        }
+      }
+    } finally {
+      $env:path = $old_path
+    }
+  }
+
+Optimize-PowershellAssemblies
